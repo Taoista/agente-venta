4a8ac4567a641995705b75fa89709921a67fa75b
taoista <taoista-@hotmail.com>
2025-02-10 09:14:01 -0300

    test proyecto enviroments

--- src/app/domains/shared/services/send-chat.service.ts
+++ src/app/domains/shared/services/send-chat.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { environment } from '@env/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class SendChatService {
     private http = inject(HttpClient);
   
     // private url = 'http://backend-agente-venta.test/api/neumaticos';
-    private url = '';
+    private url = `${environment.API_URL}ddf2f7c0-ba5d-469b-9137-af1d6bb6df01`;
    
     constructor() { }
   
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    API_URL:'https://taoistadev.app.n8n.cloud/webhook'
+};
